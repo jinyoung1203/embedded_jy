2b03f2b518ec6f10b5db691c93d57e9fb116d2e5
ITSC <ITSC@DESKTOP-SBHN298>
2023-01-10 12:43:42 +0900

    Ex0110_work

---
+++ Ex_0110/src/Ex2_work/Ex2_work.java
@@ -0,0 +1,18 @@
+package Ex2_work;
+
+public class Ex2_work {
+	public static void main(String[] args) {
+		//키보드에서 입력받은 값 만큼의 행과 열을 가지는 홀수마방진만들기
+		//위 오른쪽
+		//겹치면 바로아래
+		
+		//사이즈 : 3
+		//08 01 06
+		//03 05 07
+		//04 09 02
+		
+		
+		
+		
+	}//main
+}
