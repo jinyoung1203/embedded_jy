8f482efa161aa13c5a91e2eb42a59bcbd6586158
ITSC <ITSC@DESKTOP-SBHN298>
2023-01-16 11:51:38 +0900

    Ex_0113

---
+++ Ex_0113/src/Ex5_work/Can.java
@@ -0,0 +1,24 @@
+package Ex5_work;
+
+public class Can {
+
+	private String canName;
+	private int price;
+	
+	public String getCanName() {
+		return canName;
+	}
+	public void setCanName(String canName) {
+		this.canName = canName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+}
